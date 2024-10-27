d6783af9e3d78ad0a86e24a30cb77923dc88a6a3
gavinarori <arorigavin@gmail.com>
2024-10-27 13:25:39 +0300

    added: graphql

---
+++ graphql/typeDefs.ts
@@ -0,0 +1,29 @@
+import { gql } from 'apollo-server-express';
+
+const typeDefs = gql`
+  type User {
+    id: Int!
+    email: String!
+    isVerified: Boolean!
+    role: String!
+  }
+
+  type AuthPayload {
+    token: String
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    registerUser(email: String!, password: String!): AuthPayload
+    loginUser(email: String!, password: String!): AuthPayload
+    verifyAccount(email: String!, otp: String!): User
+    requestPasswordReset(email: String!): Boolean
+    resetPassword(token: String!, password: String!): Boolean
+  }
+`;
+
+export default typeDefs;
